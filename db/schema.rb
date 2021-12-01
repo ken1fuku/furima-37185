a4e82dd8aa52ba6078cd41de7562d51b23f3e9d6
ken1fuku <ken1fuku2012@yahoo.co.jp>
2021-12-01 15:37:31 +0900

    エラーハンドリング実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_025635) do
+ActiveRecord::Schema.define(version: 2021_11_30_075407) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nicname", null: false
+    t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
