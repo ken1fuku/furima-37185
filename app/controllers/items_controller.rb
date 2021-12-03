6b5c730b68c59adbc3ca0a5c6520e3612a47653c
ken1fuku <ken1fuku2012@yahoo.co.jp>
2021-12-03 19:16:28 +0900

    商品登録実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,23 @@ class ItemsController < ApplicationController
   def index
   end
 
-  private
+  def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(item_params)
+#  binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
   def item_params
-    params.require(:item).permit(:name, :description, :category_id,, :status_id, :charge_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :charge_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name        { Faker::Name.name }
+    description { Faker::Lorem.sentence }
+    category_id { "1a#{Faker::Internet.password(min_length: 6)}" }
+    status_id
+    charge_id             { '田中' }
+    area_id             { '田中' }
+    day_id        { 'カナ' }
+    price       { 'カナ' }
+    birthday              { '1930-01-01' }
   end
 end
